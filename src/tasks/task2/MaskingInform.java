73a6e75a67d097716e02c4dd75255ac552ff666c
AlexeyyyGG <leonovalexey555@gmail.com>
2025-04-01 09:51:56 +0300

    first commit

--- src/tasks/task2/MaskingInform.java
+++ src/tasks/task2/MaskingInform.java
@@ -4,6 +4,7 @@ package tasks.task2;
 import java.util.Scanner;
 
 public class MaskingInform {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +17,8 @@ public class MaskingInform {
         String subStr = text.substring(text.indexOf("<"), text.indexOf(">") + 1);
         return text.replace(subStr, placeholder);
     }
-
-
 }
+
+// Изменить метод
+//String searchAndReplaceDiamonds(String text, String placeholder),
+//который будет заменять в переданной строке String text всё содержимое скобок <> и сами скобки на переданную строку в String placeholder, используя регулярные выражения.
