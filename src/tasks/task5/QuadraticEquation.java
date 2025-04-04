2a1f9ff2364739ac992e47486584648e610822ca
AlexeyyyGG <leonovalexey555@gmail.com>
2025-04-04 18:49:05 +0300

    доработал QE

--- src/tasks/task5/QuadraticEquation.java
+++ src/tasks/task5/QuadraticEquation.java
@@ -5,31 +5,35 @@ import java.util.Scanner;
 public class QuadraticEquation {
 
     public static void main(String[] args) {
-        System.out.println("Введите коэффициент а: ");
-        Scanner scannera = new Scanner(System.in);
-        double a = scannera.nextDouble();
-        System.out.println("Введите коэффициент b: ");
-        Scanner scannerb = new Scanner(System.in);
-        double b = scannerb.nextDouble();
-        System.out.println("Введите коэффициент c: ");
-        Scanner scannerc = new Scanner(System.in);
-        double c = scannerc.nextDouble();
+        Scanner scanner = new Scanner(System.in);
 
-        double d = Math.pow(b,2) - 4.0 * a * c ;
-        if(d < 0){
-            System.out.println("Нет корней");
-        }else if(d == 0) {
-            double x = -b / (2 * a);
-            System.out.println("Один корень: " + x);
-        }else {
-            double x1 = (-b - Math.sqrt(d)) / (2 * a);
-            double x2 = (-b + Math.sqrt(d)) / (2 * a);
-            System.out.println("Два корня: " + x1 + " " + x2);
+        do {
+            System.out.println("Введите коэффициент а: ");
+            double a = scanner.nextDouble();
+            System.out.println("Введите коэффициент b: ");
+            double b = scanner.nextDouble();
+            System.out.println("Введите коэффициент c: ");
+            double c = scanner.nextDouble();
+            double d = Math.pow(b, 2) - 4.0 * a * c;
+
+            if (d < 0) {
+                System.out.println("Нет корней");
+            } else if (d == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Один корень: " + x);
+            } else {
+                double x1 = (-b - Math.sqrt(d)) / (2 * a);
+                double x2 = (-b + Math.sqrt(d)) / (2 * a);
+                System.out.println("Два корня: " + x1 + " " + x2);
+            }
+            System.out.println("Хотите выйти? Y/N");
+                if (scanner.nextLine().equalsIgnoreCase("Y")) {
+                    break;
+            }
+        }while(!scanner.nextLine().equalsIgnoreCase("Y"));
 
 
-        }
 
     }
-
 
 }
