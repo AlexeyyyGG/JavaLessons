6f8635f9738c5b9a71084eb3c8c8e935ab6bcb6b
AlexeyyyGG <leonovalexey555@gmail.com>
2025-04-08 14:58:02 +0300

    add single responsebility

--- src/tasks/task5/Loader.java
+++ src/tasks/task5/Loader.java
@@ -1,8 +1,25 @@
 package tasks.task5;
 
+import java.util.Scanner;
+
 public class Loader {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         QuadraticEquation equation = new QuadraticEquation();
-        equation.calculation();
+        do {
+            System.out.println("Введите коэффициент а: ");
+            double a = scanner.nextDouble();
+            System.out.println("Введите коэффициент b: ");
+            double b = scanner.nextDouble();
+            System.out.println("Введите коэффициент c: ");
+            double c = scanner.nextDouble();
+            System.out.println(equation.calculation(a, b, c));
+            System.out.println("Хотите выйти?: ");
+            if (scanner.nextLine().equalsIgnoreCase("Y")) {
+                break;
+            }
+
+        } while (!scanner.nextLine().equalsIgnoreCase("Y"));
+
     }
 }
--- src/tasks/task5/QuadraticEquation.java
+++ src/tasks/task5/QuadraticEquation.java
@@ -1,35 +1,32 @@
 package tasks.task5;
 
-import java.util.Scanner;
-
 public class QuadraticEquation {
-    public void calculation() {
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Введите коэффициент а: ");
-            double a = scanner.nextDouble();
-            System.out.println("Введите коэффициент b: ");
-            double b = scanner.nextDouble();
-            System.out.println("Введите коэффициент c: ");
-            double c = scanner.nextDouble();
-            double d = Math.pow(b, 2) - 4.0 * a * c;
+    public String noRoots() {
+        return "нет корней";
+    }
 
-            if (d < 0) {
-                System.out.println("Нет корней");
-            } else if (d == 0) {
-                double x = -b / (2 * a);
-                System.out.println("Один корень: " + x);
-            } else {
-                double x1 = (-b - Math.sqrt(d)) / (2 * a);
-                double x2 = (-b + Math.sqrt(d)) / (2 * a);
-                System.out.println("Два корня: " + x1 + " " + x2);
-            }
-            System.out.println("Хотите выйти? Y/N");
-            if (scanner.nextLine().equalsIgnoreCase("Y")) {  // условие выхода из цикла, без этого условия цикл работает дальше
-                break;
-            }
-        } while (!scanner.nextLine().equalsIgnoreCase("Y")); // если ввод от пользователя  "У" - заверши программу
+    public String oneRoot(double x) {
+        return "Один корень" + x;
+    }
+
+    public String twoRoots(double x1, double x2) {
+        return "Два корня: " + x1 + " " + x2;
+    }
+
+    public String calculation(double a, double b, double c) {
+        double d = Math.pow(b, 2) - 4.0 * a * c;
+
+        if (d < 0) {
+            return noRoots();
+        } else if (d == 0) {
+            double x = -b / (2 * a);
+            return oneRoot(x);
+        } else {
+            double x1 = (-b - Math.sqrt(d)) / (2 * a);
+            double x2 = (-b + Math.sqrt(d)) / (2 * a);
+            return twoRoots(x1, x2);
+        }
+
     }
 }
-
 
